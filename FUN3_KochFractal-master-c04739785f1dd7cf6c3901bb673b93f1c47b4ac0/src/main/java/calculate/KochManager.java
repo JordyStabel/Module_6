5aea7cca35e82940ac99ea4ffe6241c7b459f5d7
Stabel,Jordy J <j.stabel@student.fontys.nl>
2018-04-24 10:22:34 +0200

    Kochfractal is semi working

--- FUN3_KochFractal-master-c04739785f1dd7cf6c3901bb673b93f1c47b4ac0/src/main/java/calculate/KochManager.java
+++ FUN3_KochFractal-master-c04739785f1dd7cf6c3901bb673b93f1c47b4ac0/src/main/java/calculate/KochManager.java
@@ -5,6 +5,8 @@
 package calculate;
 
 import java.util.ArrayList;
+import java.util.concurrent.Callable;
+
 import fun3kochfractalfx.FUN3KochFractalFX;
 import jdk.nashorn.internal.runtime.Debug;
 import timeutil.TimeStamp;
@@ -14,10 +16,19 @@ import timeutil.TimeStamp;
  * @author Nico Kuijpers
  * Modified for FUN3 by Gertjan Schouten
  */
-public class KochManager {
+public abstract class KochManager implements Callable<Edge> {
 
     private KochFractal koch;
+
+    private KochFractal kochLeft;
+    private KochFractal kochRight;
+    private KochFractal kochBottom;
+
     private ArrayList<Edge> edges;
+
+    private ArrayList<Edge> edgesLeft;
+    private ArrayList<Edge> edgesRight;
+    private ArrayList<Edge> edgesBottom;
 
     private FUN3KochFractalFX application;
     private TimeStamp tsCalc;
@@ -26,7 +37,16 @@ public class KochManager {
     public KochManager(FUN3KochFractalFX application) {
         this.edges = new ArrayList<Edge>();
 
-        this.koch = new KochFractal(this);
+        this.edgesLeft = new ArrayList<Edge>();
+        this.edgesRight = new ArrayList<Edge>();
+        this.edgesBottom = new ArrayList<Edge>();
+
+        //this.koch = new KochFractal(this);
+
+        kochLeft = new KochFractal();
+        kochRight = new KochFractal();
+        kochBottom = new KochFractal();
+
         this.application = application;
         this.tsCalc = new TimeStamp();
         this.tsDraw = new TimeStamp();
@@ -43,7 +63,7 @@ public class KochManager {
             public void run() {
                 try
                 {
-                    koch.generateLeftEdge();
+                    kochLeft.generateLeftEdge();
                 }
                 catch (Exception e)
                 {
@@ -58,7 +78,7 @@ public class KochManager {
             public void run() {
                 try
                 {
-                    koch.generateRightEdge();
+                    kochRight.generateRightEdge();
                 }
                 catch (Exception e)
                 {
@@ -73,7 +93,7 @@ public class KochManager {
             public void run() {
                 try
                 {
-                    koch.generateBottomEdge();
+                    kochBottom.generateBottomEdge();
                 }
                 catch (Exception e)
                 {
@@ -97,6 +117,9 @@ public class KochManager {
         {
             System.out.println(ex.toString());
         }
+        thread1.interrupt();
+        thread2.interrupt();
+        thread3.interrupt();
         tsCalc.setEnd("End calculating");
         application.setTextNrEdges("" + koch.getNrOfEdges());
         application.setTextCalc(tsCalc.toString());
